589426a3cd867ba06e5509ca27be08fc5215d43d
Tejas Burate <tburate10@gmail.com>
2025-06-24 13:10:31 +0530

    initial push and setup

---
+++ src/main/java/com/attendance_services/attendance_services/AttendanceServicesApplication.java
@@ -0,0 +1,13 @@
+package com.attendance_services.attendance_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attendance_services/attendance_services/AttendanceServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance_services.attendance_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
